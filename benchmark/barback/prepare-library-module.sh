e2f25d633d9015d20691e0549925c1705830a00d
Rajeh Taher <rajeh@reforward.dev>
2026-08-11 02:05:33 +0300

    fix(bench): support legacy module baselines

---
+++ benchmark/barback/prepare-library-module.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -eu
+
+library_dir=${1:?library directory is required}
+module_path=$(sed -n 's/^module[[:space:]]\{1,\}//p' "$library_dir/go.mod" | head -n 1)
+
+case "$module_path" in
+	github.com/polymorfa/hypermeow)
+		go mod edit -replace=github.com/polymorfa/hypermeow="$library_dir"
+		;;
+	go.mau.fi/whatsmeow)
+		find cmd -type f -name '*.go' -exec sed -i 's#github.com/polymorfa/hypermeow#go.mau.fi/whatsmeow#g' {} +
+		go mod edit -dropreplace=github.com/polymorfa/hypermeow
+		go mod edit -droprequire=github.com/polymorfa/hypermeow
+		go mod edit -require=go.mau.fi/whatsmeow@v0.0.0
+		go mod edit -replace=go.mau.fi/whatsmeow="$library_dir"
+		;;
+	*)
+		echo "unsupported library module: $module_path" >&2
+		exit 2
+		;;
+esac
